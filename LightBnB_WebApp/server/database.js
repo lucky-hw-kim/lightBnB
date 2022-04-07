4a20fbbd5058eeb96e3de9d6947a7f43c26081be
Lucky Kim <lucky.hw.kim@gmail.com>
2022-04-07 14:59:52 +0000

    refactor getAllReservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -89,7 +89,27 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.*
+        FROM reservations 
+        JOIN properties 
+        ON reservations.property_id = properties.id 
+        JOIN property_reviews ON property_reviews.property_id = properties.id
+          WHERE reservations.guest_id = $1
+            GROUP BY reservations.id, 
+            properties.title, 
+            reservations.start_date, 
+            properties.cost_per_night
+      ORDER BY start_date
+      LIMIT $2;
+    `,[guest_id, limit])
+    .then((result) => {
+      return result.rows
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
 }
 exports.getAllReservations = getAllReservations;
 
